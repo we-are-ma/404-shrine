f47212f6147988b270c7597d7e2ab85c780bb2e0
HIDEKAZU AOSHIMA <h.aoshima@livlog.jp>
2021-10-12 08:02:43 +0900

    no message

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,8 @@
-const omikuji_min = 1;
-const omikuji_max = 50;
-const omikuji_num = Math.floor( Math.random() * (omikuji_max + 1 - omikuji_min) ) + omikuji_min;
-const omikuji_ret = ( '00' + omikuji_num ).slice( -2 );
-const omikuji_elem = document.getElementById("omikuji");
-omikuji_elem.src = `https://www.cotogoto.ai/omikuji/${omikuji_ret}.jpg`;
+function omikuji() {
+    const omikuji_min = 1;
+    const omikuji_max = 50;
+    const omikuji_num = Math.floor( Math.random() * (omikuji_max + 1 - omikuji_min) ) + omikuji_min;
+    const omikuji_ret = ( '00' + omikuji_num ).slice( -2 );
+    $("#omikuji").attr('src', `https://www.cotogoto.ai/omikuji/${omikuji_ret}.jpg`);
+    $("#omikuji").show();
+}
