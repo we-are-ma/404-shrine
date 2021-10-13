fa8a6a810f766fb74dd70c2795a252c659f901fa
HIDEKAZU AOSHIMA <h.aoshima@livlog.jp>
2021-10-13 23:09:16 +0900

    no message

--- docs/js/script.js
+++ docs/js/script.js
@@ -7,10 +7,52 @@ function omikuji() {
     $("#omikuji").show();
 }
 
-function takiage() {
+async function takiage() {
+
+    $('#contents').width($('body').width());
+
+    const response = await fetch('https://protopedia.net/api/prototypes.json?status=4')
+    const list = await response.json(); 
+    const uniqueList =[];
+    let prev = '';
+    for (data of list) {
+        if (prev !== data.id) {
+            uniqueList.push(data);
+        }
+        prev = data.id;
+    }
+
+    shuffle(uniqueList);
+    console.log(uniqueList[0]);
+
+    $("#prototypeLink").attr('href', 'https://protopedia.net/prototype/' + uniqueList[0].id);
+    $("#prototypeNm").text(uniqueList[0].prototypeNm);
+    if (uniqueList[0].image1.length > 0) {
+        $("#prototypeImg").attr('src', uniqueList[0].image1);
+        $("#prototypeImg").show();
+    }
+    const messages =[
+        '新たな出会い、よき縁に・・・'
+    ]
+    shuffle(messages);
+    $("#message").text(messages[0]);
+
     $("#takiage").show();
 }
 
 function link() {
     window.parent.postMessage(JSON.stringify({link: true}), "*");
+}
+
+function shuffle(arr) {
+    var n = arr.length;
+    var temp = 0, i = 0;
+
+    while (n) {
+        i = Math.floor(Math.random() * n--);
+        temp = arr[n];
+        arr[n] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
 }
